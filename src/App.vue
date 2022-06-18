2ca026596f28a118f5f8230540ad242d1aa81ea1
unknown <janehopeodonnell@gmail.com>
2022-06-18 10:57:41 -0700

    add variables file and basic styling

--- src/App.vue
+++ src/App.vue
@@ -15,24 +15,10 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import "@/assets/styles/variables.scss";
+
+body {
+  background-color: $main-background-colour;
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
