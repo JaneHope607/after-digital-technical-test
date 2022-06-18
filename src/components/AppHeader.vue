24ccba02932f59adbbe54b008548467edeb7f934
unknown <janehopeodonnell@gmail.com>
2022-06-18 10:33:26 -0700

    add assets for header and add header component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        <img alt="After Digital logo" src="@/assets/ad_logo.png" height="50">
+        <nav class="navbar">
+            <ul>
+                <li><router-link to="/"><img alt="login" src="@/assets/user.svg">Sign In</router-link></li>
+                <li><router-link to="/"><img alt="basket" src="@/assets/basket.svg">Basket</router-link></li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<style lang="scss">
+
+</style>
