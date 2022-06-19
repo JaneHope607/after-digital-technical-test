dc8845db61107aa99a2c49ef053bdd3097439b49
unknown <janehopeodonnell@gmail.com>
2022-06-19 02:51:04 -0700

    styling for header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,12 +1,14 @@
 <template>
-    <header>
-        <img alt="After Digital logo" src="@/assets/ad_logo.png" height="50">
-        <nav class="navbar">
+    <header class="site-header">
+         <div class="site-header__wrapper">
+            <img class="site-logo" alt="After Digital logo" src="@/assets/ad_logo.png" height="50">
+            <nav class="navbar">
             <ul>
-                <li><router-link to="/"><img alt="login" src="@/assets/user.svg">Sign In</router-link></li>
-                <li><router-link to="/"><img alt="basket" src="@/assets/basket.svg">Basket</router-link></li>
+                <li><router-link to="/"><img alt="login" class="icon" src="@/assets/user.svg">Sign In</router-link></li>
+                <li><router-link to="/"><img alt="basket" class="icon" src="@/assets/basket.svg">Basket</router-link></li>
             </ul>
-        </nav>
+            </nav>
+        </div>
     </header>
 </template>
 
@@ -16,6 +18,38 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+@import "@/assets/styles/variables.scss";
+
+* {
+    box-sizing: border-box;
+}
+
+body {
+    font-family: 'Gill Sans', sans-serif;
+    font-size: 18px;
+    margin-top: 10px;
+}
+
+.site-header {
+    height: 180px;
+    margin: 40px 20px;
+
+    &__wrapper {
+    background-color: $white;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+}
+
+.site-logo {
+    margin-left: 20px;
+}
+
+.icon {
+    width: 2rem;
+    height: auto;
+}
 
 </style>
